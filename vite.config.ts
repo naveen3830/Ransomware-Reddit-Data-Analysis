ef972410e29bdf792363d4da13669306791306bd
naveen3830 <naveenr3830@gmail.com>
2025-07-12 20:46:11 +0530

    fix: expose version to code

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,21 @@
+// vite.config.ts
 import path from 'path';
 import { defineConfig, loadEnv } from 'vite';
 import { version } from './package.json';
 
-declare const __APP_VERSION__: string;
-
 export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, '.', '');
-    return {
-      define: {
-        'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-        '__APP_VERSION__': JSON.stringify(version)
+  const env = loadEnv(mode, '.', '');
+
+  return {
+    define: {
+      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
+      __APP_VERSION__: JSON.stringify(version),
+    },
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, '.'),
       },
-      resolve: {
-        alias: {
-          '@': path.resolve(__dirname, '.'),
-        }
-      }
-    };
+    },
+  };
 });
